366df7d87e82dc72b4d4517605682fd97c8ba037
Eslam Alameldin <eashraf36@gmail.com>
2024-02-06 14:03:17 +0200

    adding coupons and shopping cart features

--- server.js
+++ server.js
@@ -8,23 +8,16 @@ dotenv.config({ path: "config.env" });
 const ApiError = require("./utils/apiError");
 const globalError = require("./middlewares/errorMiddleware");
 const dbConnection = require("./config/database");
-const categoryRoute = require("./routes/categoryRoute");
-const subCategoryRoute = require("./routes/subCategoryRoute");
-const brandRoute = require("./routes/brandRoute");
-const productRoute = require("./routes/productRoute");
-const userRoute = require("./routes/userRoute");
-const authRoute = require("./routes/authRoute");
-const reviewRoute = require("./routes/reviewRoute");
-const wishlistRoute = require("./routes/wishlistRoute");
-const addressRoute = require("./routes/addressRoute");
+// Routes
+const mountRoutes = require("./routes");
 
-// connect with db
+// Connect with db
 dbConnection();
 
 // express app
 const app = express();
 
-// middlewares
+// Middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads")));
 
@@ -33,19 +26,11 @@ if (process.env.NODE_ENV === "development") {
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-// Mounting routes
-app.use("/api/v1/categories", categoryRoute);
-app.use("/api/v1/subcategories", subCategoryRoute);
-app.use("/api/v1/brands", brandRoute);
-app.use("/api/v1/products", productRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use("/api/v1/wishlists", wishlistRoute);
-app.use("/api/v1/addresses", addressRoute);
+// Mount Routes
+mountRoutes(app);
 
 app.all("*", (req, res, next) => {
-  next(new ApiError(`Can't find ${req.originalUrl} on this server`, 404));
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
 // Global error handling middleware for express
@@ -53,14 +38,14 @@ app.use(globalError);
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`App running running on port ${PORT}`);
 });
 
 // Handle rejection outside express
 process.on("unhandledRejection", (err) => {
   console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
-    console.error("Shutting down ...");
+    console.error(`Shutting down....`);
     process.exit(1);
   });
 });
